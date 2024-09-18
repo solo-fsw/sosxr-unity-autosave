0dd4cf961ee5fd87ebc02e8f7115d910e91c18dd
mrstruijk <mrstruijk@gmail.com>
2024-09-18 15:58:53 +0200

    Some auto-formatting

--- Editor/TarodevAutoSave.cs
+++ Editor/TarodevAutoSave.cs
@@ -4,25 +4,30 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEditorInternal;
 using UnityEngine;
 
-namespace Tarodev {
+
+namespace Tarodev
+{
     /// <summary>
-    /// Unity has probably discussed an auto-save feature countless times over the years
-    /// and decided not to implement... so take that information as you'd like. I personally
-    /// like the idea and it's worked well for me during my limited testing. If you find any bugs
-    /// please report them on the repo: https://github.com/Matthew-J-Spencer/Unity-AutoSave
-    /// 
-    /// Love your friendly neighborhood Tarodev
+    ///     Unity has probably discussed an auto-save feature countless times over the years
+    ///     and decided not to implement... so take that information as you'd like. I personally
+    ///     like the idea and it's worked well for me during my limited testing. If you find any bugs
+    ///     please report them on the repo: https://github.com/Matthew-J-Spencer/Unity-AutoSave
+    ///     Love your friendly neighborhood Tarodev
     /// </summary>
     [CustomEditor(typeof(AutoSaveConfig))]
-    public class TarodevAutoSave : Editor {
+    public class TarodevAutoSave : Editor
+    {
         private static AutoSaveConfig _config;
         private static CancellationTokenSource _tokenSource;
         private static Task _task;
 
+
         [InitializeOnLoadMethod]
-        private static void OnInitialize() {
+        private static void OnInitialize()
+        {
             FetchConfig();
             CancelTask();
 
@@ -30,15 +35,23 @@ namespace Tarodev {
             _task = SaveInterval(_tokenSource.Token);
         }
 
-        private static void FetchConfig() {
-            while (true) {
-                if (_config != null) return;
+
+        private static void FetchConfig()
+        {
+            while (true)
+            {
+                if (_config != null)
+                {
+                    return;
+                }
 
                 var path = GetConfigPath();
 
-                if (path == null) {
+                if (path == null)
+                {
                     AssetDatabase.CreateAsset(CreateInstance<AutoSaveConfig>(), $"Assets/{nameof(AutoSaveConfig)}.asset");
                     Debug.Log("A config file has been created at the root of your project.<b> You can move this anywhere you'd like.</b>");
+
                     continue;
                 }
 
@@ -48,40 +61,75 @@ namespace Tarodev {
             }
         }
 
-        private static string GetConfigPath() {
+
+        private static string GetConfigPath()
+        {
             var paths = AssetDatabase.FindAssets(nameof(AutoSaveConfig)).Select(AssetDatabase.GUIDToAssetPath).Where(c => c.EndsWith(".asset")).ToList();
-            if (paths.Count > 1) Debug.LogWarning("Multiple auto save config assets found. Delete one.");
+
+            if (paths.Count > 1)
+            {
+                Debug.LogWarning("Multiple auto save config assets found. Delete one.");
+            }
+
             return paths.FirstOrDefault();
         }
 
-        private static void CancelTask() {
-            if (_task == null) return;
+
+        private static void CancelTask()
+        {
+            if (_task == null)
+            {
+                return;
+            }
+
             _tokenSource.Cancel();
             _task.Wait();
         }
 
-        private static async Task SaveInterval(CancellationToken token) {
-            while (!token.IsCancellationRequested) {
-                await Task.Delay(_config.Frequency * 1000 * 60, token);
-                if (_config == null) FetchConfig();
 
-                if (!_config.Enabled || Application.isPlaying || BuildPipeline.isBuildingPlayer || EditorApplication.isCompiling) continue;
-                if (!UnityEditorInternal.InternalEditorUtility.isApplicationActive) continue;
+        private static async Task SaveInterval(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(_config.Frequency * 1000 * 60, token);
+
+                if (_config == null)
+                {
+                    FetchConfig();
+                }
+
+                if (!_config.Enabled || Application.isPlaying || BuildPipeline.isBuildingPlayer || EditorApplication.isCompiling)
+                {
+                    continue;
+                }
+
+                if (!InternalEditorUtility.isApplicationActive)
+                {
+                    continue;
+                }
 
                 EditorSceneManager.SaveOpenScenes();
-                if (_config.Logging) Debug.Log($"Auto-saved at {DateTime.Now:h:mm:ss tt}");
+
+                if (_config.Logging)
+                {
+                    Debug.Log($"Auto-saved at {DateTime.Now:h:mm:ss tt}");
+                }
             }
         }
-        
+
+
         [MenuItem("Window/Auto save/Find config")]
-        public static void ShowConfig() {
+        public static void ShowConfig()
+        {
             FetchConfig();
 
             var path = GetConfigPath();
             EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<AutoSaveConfig>(path).GetInstanceID());
         }
 
-        public override void OnInspectorGUI() {
+
+        public override void OnInspectorGUI()
+        {
             DrawDefaultInspector();
             EditorGUILayout.Space();
             EditorGUILayout.HelpBox("You can move this asset where ever you'd like.\nWith ❤, Tarodev.", MessageType.Info);
